0035337461a094c2f8c8d86b71037fd1006e06d9
Carlos Portes <carlosportes.c@gmail.com>
2022-12-06 08:22:26 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, value, next_node=None):
         self.value = value
         self.next_node = next_node
+        self.power = value**2
 
     def set_next_node(self, next_node):
         self.next_node = next_node
